d16ea717174ce4b2cbe1233b29c041a8bce37d70
KimaeWoo <134660009+KimaeWoo@users.noreply.github.com>
2024-01-17 00:00:37 +0900

    3주차 django 개발 시작

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SignConfig(AppConfig):
+class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sign'
+    name = 'accounts'
